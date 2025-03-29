81a3ef93a40c260b78f3b936159f5dde6a3cd33b
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-03-28 22:50:00 -0300

    routes

---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express'); 
+const router = express.Router(); 
+ 
+const UsuariosController = require('../controllers/usuarios'); 
+
+router.get('/usuarios', UsuariosController.listarUsuarios); 
+
+module.exports = router;
